4d6d09f404020d547e907c332e8d2f2b3114435e
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-02-09 16:56:30 +0530

    Update ProductController.java

--- NimapProject/Updated/SpringBootProject/src/main/java/com/SpringBoot/CategoryProduct/Controller/ProductController.java
+++ NimapProject/Updated/SpringBootProject/src/main/java/com/SpringBoot/CategoryProduct/Controller/ProductController.java
@@ -1,8 +1,10 @@
 package com.SpringBoot.CategoryProduct.Controller;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,21 +18,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.SpringBoot.CategoryProduct.Entity.Product;
-import com.SpringBoot.CategoryProduct.Exceptions.CategoryException;
 import com.SpringBoot.CategoryProduct.Exceptions.ProductExceptions;
-import com.SpringBoot.CategoryProduct.Service.CategoryService;
 import com.SpringBoot.CategoryProduct.Service.ProductService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class ProductController {
 
 	@Autowired
 	ProductService productservice;
-
-	@Autowired
-	CategoryService categoryservice;
 
 	/*
 	 * Adding the product using post mapping localhost:8080/api/products
@@ -45,30 +41,33 @@ public class ProductController {
 	 * Retrieved the all products using GetMapping annotation with the help of Pagination 
 	 * localhost:8080/api/products?page=1 get the 3 product in one page
 	 */
-	@GetMapping("/products")
-	public ResponseEntity<List<Product>> getProduct(@RequestParam(defaultValue = "0") int page,
-			@RequestParam(defaultValue = "3") int size) throws ProductExceptions {
-		List<Product> product = productservice.getProductUsingPaging(page, size);
-
-		if (product.size() <= 0) {
-			throw new ProductExceptions("Product not exist");
+        @GetMapping("/products")
+	public Page<Product> getProduct(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "3") int size) {
+		if (size <= 0) {
+			throw new ProductExceptions("Category not exist");
 		}
-		return ResponseEntity.of(Optional.of(product));
+
+		return productservice.getProductUsingPaging(PageRequest.of(page, size));
 	}
 
 	/*
 	 * sorting implementation In product or show the product with sorted productName
 	 * localhost:8080/api/products/sorting
 	 */
-	@GetMapping("/products/sorting")
-	public ResponseEntity<Iterable<Product>> getSortProduct() throws ProductExceptions {
-
-		List<Product> product = (List<Product>) productservice.getSortProductUsingSorting();
-
-		if (product.size() <= 0) {
-			throw new CategoryException("Product not exist");
+	@GetMapping("/products/sort")
+	public Page<Product> getProductUsingSorting(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "3") int size) {
+		if (size <= 0) {
+			throw new ProductExceptions("Product not exist");
 		}
-		return ResponseEntity.of(Optional.of(product));
+		String sortBy = "productName";
+		
+		Sort sort = Sort.by(Sort.Direction.ASC, sortBy);
+		
+		Pageable pageable = PageRequest.of(page, size, sort);
+		
+		return productservice.getProductUsingSorting(pageable);
 	}
 
 	/*
